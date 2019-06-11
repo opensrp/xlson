0232bba9465dc6b3d570375cce207fa23cb96c73
WinnyTroy <kiraguwinnie@gmail.com>
2019-06-11 16:59:35 +0300

    Include other supported question types

--- xlson/__init__.py
+++ xlson/__init__.py
@@ -22,7 +22,7 @@ TITLE = "title"
 TYPE = "type"
 CONSTRAINT = "constraint"
 REQUIRED = "required"
-SUPPORTED_QUESTIONS_TYPES = ["text", "group"]
+SUPPORTED_QUESTIONS_TYPES = ["barcode", "geopoint", "group", "integer", "photo", "text", "select one", "select all that apply"]
 
 BIND_CONVERSTION = {"yes": "true", "Yes": "true", "no": "false", "No": "false"}
 
