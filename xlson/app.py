df0e7cb66548519318b074312a2f23f129a81144
michaelmurwayi <mmurwayi@ona.io>
2019-08-26 17:44:38 +0300

    class view for file upload

--- xlson/app.py
+++ xlson/app.py
@@ -1,16 +1,22 @@
 """ Importing required strings """
 import os
+from typing import Any
 
 from django import forms
+from django.conf import settings
+from django.urls import reverse_lazy
 from django.views.generic.edit import FormView
-from django_micro import configure, run
-
-DEBUG = True
-configure(locals())
-
+from django_micro import configure, route, run
 
 CONTENT_TYPES = [".xlsx", ".xls"]
 MAX_UPLOAD_SIZE = 10485760
+FILETYPE_ERROR = "wrong file type"
+FILESIZE_ERROR = "file too large"
+SUCCESFULL_UPLOAD = "File has been uploaded"
+
+
+DEBUG = True
+configure(locals())
 
 
 class UploadFile(forms.Form):
@@ -18,25 +24,28 @@ class UploadFile(forms.Form):
 
     files = forms.FileField()
 
-    def clean_files(self) -> str:
+    def clean_files(self) -> Any:
         """clean data recieved from the upload form """
+        content_types = settings.CONTENT_TYPES
+        upload_size = settings.MAX_UPLOAD_SIZE
         files = self.cleaned_data.get("files")
         ext = os.path.splitext(files.name)[-1]
         file_size = files.size
-        if ext in CONTENT_TYPES:
-            if file_size >= MAX_UPLOAD_SIZE:
-                raise Exception("file too large")
+        if ext in content_types:
+            if file_size >= upload_size:
+                raise Exception(settings.FILESIZE_ERROR)
         else:
-            raise Exception("wrong file type")
-        return "uploaded"
+            raise Exception(settings.FILETYPE_ERROR)
+        return settings.SUCCESFULL_UPLOAD
 
 
+@route("", name="UploadFile")  # pylint: disable=too-many-ancestors
 class UploadView(FormView):  # pylint: disable=too-many-ancestors
     """ upload view """
 
     template_name = "upload.html"
     form_class = UploadFile
-    success_url = "/thanks/"
+    success_url = reverse_lazy("UploadFile")
 
 
 APPLICATION = run()
