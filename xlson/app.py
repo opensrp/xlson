a09219b9f3fbaddb3a23162bfe8b8b8074ed5d28
michaelmurwayi <mmurwayi@ona.io>
2019-08-26 12:48:57 +0300

    class view for file upload

--- xlson/app.py
+++ xlson/app.py
@@ -1,37 +1,42 @@
 """ Importing required strings """
-from typing import Any
+import os
 
 from django import forms
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.views.generic.edit import FormView
 from django_micro import configure, run
 
 DEBUG = True
 configure(locals())
 
 
-class UploadFileForm(forms.Form):
-    """ models for uploaded file """
+CONTENT_TYPES = [".xlsx", ".xls"]
+MAX_UPLOAD_SIZE = 10485760
+
+
+class UploadFile(forms.Form):
+    """field in the upload form"""
 
     files = forms.FileField()
 
+    def clean_files(self) -> str:
+        """clean data recieved from the upload form """
+        files = self.cleaned_data.get("files")
+        ext = os.path.splitext(files.name)[-1]
+        file_size = files.size
+        if ext in CONTENT_TYPES:
+            if file_size >= MAX_UPLOAD_SIZE:
+                raise Exception("file too large")
+        else:
+            raise Exception("wrong file type")
+        return "uploaded"
 
-def upload_file(request: HttpRequest) -> HttpResponse:
-    """ handle all uploads """
-    if request.method == "POST":
-        # get the uploaded file through the uploadFileform model
-        form: Any = UploadFileForm(request.POST, request.FILES)
 
-        # get the file extension
-        ext: str = request.FILES.get("form").name.split(".")[-1]
+class UploadView(FormView):  # pylint: disable=too-many-ancestors
+    """ upload view """
 
-        # validate file is xlsx or xls
-        if ext not in ("xlsx", "xls"):
-            render(request, "upload.html", status=400)
-        render(request, "upload.html", {"form": form})
-    else:
-        form = UploadFileForm()
-    return render(request, "upload.html", {"form": form})
+    template_name = "upload.html"
+    form_class = UploadFile
+    success_url = "/thanks/"
 
 
 APPLICATION = run()
