08c556aa540cc0682adc1cc4dcc183c65af252f6
michaelmurwayi <mmurwayi@ona.io>
2019-08-29 17:05:54 +0300

    saving and reading uploaded file

--- xlson/app.py
+++ xlson/app.py
@@ -2,8 +2,10 @@
 import os
 from typing import Any
 
+import pandas as pd
 from django import forms
 from django.conf import settings
+from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic.edit import FormView
 from django_micro import configure, route, run
@@ -14,6 +16,9 @@ FILETYPE_ERROR = "wrong file type"
 FILESIZE_ERROR = "file too large"
 SUCCESFULL_UPLOAD = "File has been uploaded"
 UPLOAD_FILES = "files"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MEDIA_ROOT = os.path.join(BASE_DIR, "xlson/uploaded_files/")
+MEDIA_URL = "upload_-files/"
 
 
 DEBUG = True
@@ -27,16 +32,15 @@ class UploadFile(forms.Form):
 
     def clean_files(self) -> Any:
         """clean data recieved from the upload form """
-
         files = self.cleaned_data.get(settings.UPLOAD_FILES)
         ext = os.path.splitext(files.name)[-1]
         file_size = files.size
         if ext in settings.EXTENSION_TYPES:
             if file_size >= settings.MAX_UPLOAD_SIZE:
-                raise Exception(settings.FILESIZE_ERROR)
+                raise forms.ValidationError(settings.FILESIZE_ERROR)
         else:
-            raise Exception(settings.FILETYPE_ERROR)
-        return settings.SUCCESFULL_UPLOAD
+            raise forms.ValidationError(settings.FILETYPE_ERROR)
+        return files
 
 
 @route("", name="UploadFile")  # pylint: disable=too-many-ancestors
@@ -46,6 +50,24 @@ class UploadView(FormView):  # pylint: disable=too-many-ancestors
     template_name = "upload.html"
     form_class = UploadFile
     success_url = reverse_lazy("UploadFile")
+
+    def form_valid(self, form: Any) -> HttpResponse:
+        """ handle post requests """
+        output = self.handle_uploaded_file(form.files["files"])
+        print(output)
+        return HttpResponse("file succesfully processed")
+
+    def handle_uploaded_file(self, files: Any) -> Any:  # pylint: disable=R0201
+        """ Read the uploaded xls file"""
+        import ipdb
+
+        ipdb.set_trace()
+        new_file = os.path.join(settings.MEDIA_ROOT, files.name)
+        with open(new_file, "wb+") as destination:
+            for chunk in files.chunks():
+                destination.write(chunk)
+        workbook = pd.read_excel(open(new_file, "rb"))
+        return workbook
 
 
 APPLICATION = run()
